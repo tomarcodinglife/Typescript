7053cf8c59dc661ddc7c30c38421b6d6dd693add
Sujit Tomar <tomarcodinglife@gmail.com>
2026-05-21 07:59:55 +0530

    updated

--- 01_Basics/01_basics.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let myName = "Sujit Tomar";
-let Age = 25;
-let myAge = 25; // TypeScript can infer the type as number
-myName = "Sujit Kumar Tomar";
-Age = 26;
-
-console.log(myName);
-console.log(Age);
