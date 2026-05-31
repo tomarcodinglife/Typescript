6b0d05c96d5691849216fb8f25d8fef1adc4b2e1
Sujit Tomar <tomarcodinglife@gmail.com>
2026-05-31 20:48:52 +0530

    updated

--- 26_keyOf/keyOf.ts
+++ 26_keyOf/keyOf.ts
@@ -12,7 +12,33 @@ let myInfo: personABC = {
 };
 
 type person001 = keyof personABC;
-let  myPersonData : person001;
+// let  myPersonData : person001; // or
+let  myPersonData : keyof personABC;
+
+myPersonData = "age";
+myPersonData = "isEmp";
+myPersonData = "newName";
 
 
+let newUserA : keyof typeof myInfo = "age"
 
+console.log(newUserA);
+
+function myDataGetFun (obj: typeof myInfo, key: keyof typeof myInfo){
+    return obj[key];
+}
+
+function myDataUpdateFun(field: keyof typeof myInfo, value: string | number){
+    // myInfo[field] = value; // Type 'string | number' is not assignable to type 'never'. Type 'string' is not assignable to type 'never'.
+    console.log(field, value)
+}
+
+// Advance with generic
+function objUpdateFun<K extends keyof typeof myInfo>(field : K, value : typeof myInfo[K]){
+    myInfo[field] = value;
+}
+
+
+console.log(myInfo) // { newName: 'TypeScript Boy', age: 20, isEmp: true }
+objUpdateFun("age", 56)
+console.log(myInfo) // { newName: 'TypeScript Boy', age: 56, isEmp: true }
